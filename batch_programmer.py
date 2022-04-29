c581bb9b57f834249416838907bca36085a8967d
Geo_Pap <papageogeo91@gmail.com>
2022-04-29 15:37:15 +0300

    "Added" main menu

--- batch_programmer.py
+++ batch_programmer.py
@@ -29,7 +29,7 @@ def choose_quantity():
 		
 		print("You choose", count,"!")
 		
-		choise = input("Are you sure you need this much? (y/N)(q to Quit) :")
+		choise = input("Are you sure you need this much? (y/N)(Press Q to Quit): ")
 		match choise:
 			case "Y" | "y":
 				return count
@@ -47,7 +47,10 @@ def programming_routine(counter):	#	The input needs a int. This number gives the
 				counter -= 1
 				input("OK.Press any key to continue!")
 			case _:
-				input("Something gone wrong :(")
+				choise = input("Something gone wrong! (Press Any Key to Continue)(Press Q to Quit): ")
+				match choise:
+					case "Q" | "q":
+						return 1 
 
 		
 #	The actual programming of each chip
@@ -65,7 +68,7 @@ def file_find():
 	return file[0]
 
 
-#	Check if the directory that i give as attribute exists
+#	Check if the directory that I give as attributes exists
 def check_dir(dir):
 	isdir = os.path.isdir(dir)
 	match isdir:
@@ -75,12 +78,12 @@ def check_dir(dir):
 			return 1
 
 
-#	Utility function that take each kind of memory from the mcu and copies it to a folder
-def mcu_backup(where):	#	"where" is the folder that the files should be saved
-	isdir = check_dir(where)
+#	Utility function that take each kind of memory from the MCU and copies it to a folder
+def mcu_backup(restore_folder):	#	"restore_folder" is the folder that the files going to be saved
+	isdir = check_dir(restore_folder)
 	match isdir:
 		case 0:	
-			os.chdir(where)	#	change directory where you want to save the results 
+			os.chdir(restore_folder)	#	change directory restore_folder you want to save the results 
 			mem_type = ["eeprom", "flash", "signature", "lfuse", "hfuse", "efuse"]
 			for m in mem_type:
 				command = "avrdude -p "+my_mcu+" -c "+my_programmer+" -n -U "+m+":r:"+m+".hex:i"
@@ -90,18 +93,18 @@ def mcu_backup(where):	#	"where" is the folder that the files should be saved
 			choise = input("Directory doesn't exists. Do you want to create it? (y/N)")
 			match choise:
 				case "Y" | "y":
-					os.mkdir(where)
-					mcu_backup(where)
+					os.mkdir(restore_folder)
+					mcu_backup(restore_folder)
 				case _:
 					return 1
 		
 		
 #	Write the MCU's	default state in the MCU	
-def mcu_restore(default_folder):	#	"default_folder" is the folder where are saved files for the backup 
-	isdir = check_dir(default_folder)	#	Check if the folder exists
+def mcu_restore(backup_folder):	#	"backup_folder" is the folder restore_folder are the saved files from the backup 
+	isdir = check_dir(backup_folder)	#	Check if the folder exists
 	match isdir:
 		case 0:
-			os.chdir(default_folder)	#	In which folder you want to save the results 
+			os.chdir(backup_folder)	#	In which folder you want to save the results 
 			mem_type = ["eeprom", "flash", "lfuse", "hfuse", "efuse"]
 			for m in mem_type:
 				command = "avrdude -p "+my_mcu+" -c "+my_programmer+" -U "+m+":w:"+m+".hex"
@@ -110,13 +113,15 @@ def mcu_restore(default_folder):	#	"default_folder" is the folder where are save
 		case _:
 			print("The folder probably doesn't exist!")
 			return 1
+
+
+def main_menu():
+	pass
+	return 0
 	
 #	MAIN-----------------------------------------------------------------
 
-# choose_quantity()
-# print(mcu_backup("lol"))
-# mcu_backup("lol")
-# print(mcu_restore("new"))
+mcu_backup("tested")
 
-programming_routine(choose_quantity())
-input("Press Enter to quit!")
+# programming_routine(choose_quantity())
+input("Press Any Key to Quit!")
